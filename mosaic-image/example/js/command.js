0f7fa078dc78000b61e1cf79214302d7ac09346a
dimaiv95 <ivdim4ik95@gmail.com>
2017-08-17 13:47:35 +0300

    Update command.js

--- mosaic-image/example/js/command.js
+++ mosaic-image/example/js/command.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	
-	$(".mosaic-container").imageMosaic({cell: 4, marginl: 10, marginb: 10, preventD: false});
+	$(".mosaic-container").imageMosaic({cell: 4, marginl: 10, marginb: 10});
 	
 });
