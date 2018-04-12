7f86a70e21c47c79505e7013e1f7618afec902a4
dimaiv95 <ivdim4ik95@gmail.com>
2018-04-12 13:02:56 +0300

    versal

---
+++ versal/js/common.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	
+	var children;
+	
+	$(".header-menu nav > ul > li").each(function(i, t){
+		children = t.children.length
+		if(children > 1){
+			$(this).addClass("subMenuItem");
+			//$(this).children("ul").children("li").removeClass("subMenuItem");
+			$(this).children("a").on("click", function(e){
+				e.preventDefault();
+			})
+			//console.log(i, t );
+		}
+		
+	});
+	$(".header-menu nav > ul > li").on("click", function(e){
+		var submenu = $(this).children("ul");
+		if(submenu.is(":hidden")){
+			submenu.slideDown(300);
+		}
+		else{
+			submenu.slideUp(300);
+		}
+		$(".header-menu nav > ul > li").children("ul").not(submenu).slideUp(300);
+	});
+	
+	$(".header-info").owlCarousel({
+		items: 1,
+		dots: true,
+		loop: true,
+		autoplay: true
+	});
+	
+	$(".products").owlCarousel({
+		items: 5,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		responsive: {
+			768: {
+				items: 5
+			},
+			480: {
+				items: 3
+			},
+			240: {
+				items: 2
+			}
+		}
+	});
+	
+	$(".partners").owlCarousel({
+		items: 6,
+		margin: 26,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		responsive: {
+			768: {
+				items: 5
+			},
+			320: {
+				items: 3	
+			},
+			240: {
+				items: 2
+			}
+		}
+	});
+	
+});
