99c7762ba0453d7f7f628db10ca5f38d14de2df6
dimaiv95 <ivdim4ik95@gmail.com>
2018-04-13 20:39:05 +0300

    correct

--- versal/js/common.js
+++ versal/js/common.js
@@ -14,16 +14,21 @@ $(document).ready(function(){
 		}
 		
 	});
-	$(".header-menu nav > ul > li").on("click", function(e){
-		var submenu = $(this).children("ul");
-		if(submenu.is(":hidden")){
-			submenu.slideDown(300);
-		}
-		else{
-			submenu.slideUp(300);
-		}
-		$(".header-menu nav > ul > li").children("ul").not(submenu).slideUp(300);
-	});
+	//$(".header-menu nav > ul > li").children("ul").css({"zIndex": 100});
+	if($(window).width() <= 992){
+		$(".header-menu nav > ul > li").on("click", function(e){
+			var submenu = $(this).children("ul");
+				//submenu.css("opacity", 1);
+
+			if(submenu.is(":hidden")){
+				submenu.slideDown(300);
+			}
+			else{
+				submenu.slideUp(300);
+			}
+			$(".header-menu nav > ul > li").children("ul").not(submenu).slideUp(300);
+		});
+	}
 	
 	$(".header-info").owlCarousel({
 		items: 1,
