9df3485404710c72b1dd3c1339a76a6e3288f1d5
Dima Iv <ivdim4ik95@gmail.com>
2019-04-22 13:01:13 +0300

    add build star-db

---
+++ react/star-db/precache-manifest.e84883478672334b3724cf39627800ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00da8937911caa3e855b7999794cc122",
+    "url": "./static/media/error-indicator.00da8937.png"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "b164b00ad5c8b1722508",
+    "url": "./static/js/main.b399bfda.chunk.js"
+  },
+  {
+    "revision": "1cad0f9dcf53817a752e",
+    "url": "./static/js/2.78d71bba.chunk.js"
+  },
+  {
+    "revision": "b164b00ad5c8b1722508",
+    "url": "./static/css/main.0b47badf.chunk.css"
+  },
+  {
+    "revision": "4363a376c9e2168b13b054a90d8bbbab",
+    "url": "./index.html"
+  }
+];
