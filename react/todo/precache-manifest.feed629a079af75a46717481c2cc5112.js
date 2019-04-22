2a5d7814c7e304ac150ffafae8ab347b46cc91f6
Dima Iv <ivdim4ik95@gmail.com>
2019-04-22 12:36:43 +0300

    ToDo

---
+++ react/todo/precache-manifest.feed629a079af75a46717481c2cc5112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "86849450b47def9de7f4",
+    "url": "./static/js/main.86849450.chunk.js"
+  },
+  {
+    "revision": "6d6a22f925a97b4246ef",
+    "url": "./static/js/2.6d6a22f9.chunk.js"
+  },
+  {
+    "revision": "86849450b47def9de7f4",
+    "url": "./static/css/main.07c4398b.chunk.css"
+  },
+  {
+    "revision": "237ec56526e977dea1f7e190b4548508",
+    "url": "./index.html"
+  }
+];
