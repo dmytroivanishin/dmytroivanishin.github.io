63d743bbf487910a638de44711811eb56aefca97
Dima Iv <ivdim4ik95@gmail.com>
2019-04-22 13:20:50 +0300

    add build re-store

---
+++ react/re-store/precache-manifest.daeba2a8b9e5a0ee13ccf6b41ab2eb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00da8937911caa3e855b7999794cc122",
+    "url": "./static/media/error-indicator.00da8937.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "3154b3810da43e066dfb",
+    "url": "./static/js/main.ee594312.chunk.js"
+  },
+  {
+    "revision": "79cef7fb1190c7b453b0",
+    "url": "./static/js/2.cfb5dd64.chunk.js"
+  },
+  {
+    "revision": "3154b3810da43e066dfb",
+    "url": "./static/css/main.dde0bb5c.chunk.css"
+  },
+  {
+    "revision": "ec1d87aab1c9712be5f557042aaa352a",
+    "url": "./index.html"
+  }
+];
