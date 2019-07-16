f4512869efc76f3fa523e771cbf30ef775d99ed5
Dima Iv <ivdim4ik95@gmail.com>
2019-07-16 15:10:13 +0300

    test life-and-photo

---
+++ build/precache-manifest.bf2c0ffdc734ffabb95cabf9f32ec5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a1c4dab1ae29007fa30df1293df660",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e7ff9aeef00d2d523cb2",
+    "url": "./static/css/main.5588d7a0.chunk.css"
+  },
+  {
+    "revision": "7114e1792a792fa62b93",
+    "url": "./static/js/2.abad947d.chunk.js"
+  },
+  {
+    "revision": "e7ff9aeef00d2d523cb2",
+    "url": "./static/js/main.b538c61a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "0d58082f1536039a7035655b610598b6",
+    "url": "./static/media/error.0d58082f.svg"
+  }
+]);
