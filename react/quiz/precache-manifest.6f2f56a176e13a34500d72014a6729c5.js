921935fce9e65acae90aca97dc8bd02001f4e6d5
Dima Iv <ivdim4ik95@gmail.com>
2020-03-28 18:44:11 +0300

    Quiz

---
+++ react/quiz/precache-manifest.6f2f56a176e13a34500d72014a6729c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0b92e9e41af44a77ab65874db8c16a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87e3289eac28519d9fef",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "54aa14f9b0389b896d24",
+    "url": "/static/css/main.3b00050a.chunk.css"
+  },
+  {
+    "revision": "87e3289eac28519d9fef",
+    "url": "/static/js/2.925442c6.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.925442c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54aa14f9b0389b896d24",
+    "url": "/static/js/main.ddeb19e2.chunk.js"
+  },
+  {
+    "revision": "fb2e049fe65ee87f6368",
+    "url": "/static/js/runtime-main.39aec195.js"
+  }
+]);
